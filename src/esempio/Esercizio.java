ea4f1b2a8cdfdcbfbb1cf6cb1883ddf58a776d2a
Asus <elenuzza91@hotmail.it>
2017-09-25 20:38:01 +0200

    Inizio

---
+++ src/esempio/Esercizio.java
@@ -0,0 +1,13 @@
+package esempio;
+
+public class Esercizio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Operazioni op = new Operazioni();
+		
+		System.out.println(op.somma(5,5));
+		System.out.println(op.sottrazione(5,5));
+	}
+
+}
---
+++ src/esempio/Operazioni.java
@@ -0,0 +1,12 @@
+package esempio;
+
+public class Operazioni {
+	
+	public int somma (int x, int y){
+		return x + y;
+	}
+
+	public int sottrazione(int x, int y){
+		return x - y;
+	}
+}
